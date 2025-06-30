7f11f5a0a0c375b130cf12e98733e4fb771201d0
junkim0 <chrisandquartz@gmail.com>
2025-06-30 11:03:35 +0900

    Test absolute simplest function approach

--- api/index.py
+++ api/index.py
@@ -1,10 +1,17 @@
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-        self.wfile.write('Hello, world! Text Complexity Estimator is working!'.encode('utf-8'))
-        return 
+def handler(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'text/html',
+        },
+        'body': '''
+        <html>
+        <head><title>Text Complexity Estimator</title></head>
+        <body>
+            <h1>SUCCESS! Text Complexity Estimator is Working!</h1>
+            <p>The site is now live and functional.</p>
+            <p>This is the minimal working version.</p>
+        </body>
+        </html>
+        '''
+    } 
